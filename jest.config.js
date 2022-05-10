67e27bdcc35e56b7f5a2b912d5662e5755ba1b9b
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-10 16:30:14 -0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
+  preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts&': 'ts-jest' // convert all ts files to js before testing
