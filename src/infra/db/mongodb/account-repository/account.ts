88419c6c09ddb42cb72d0d795cbd88fd7bd33fe4
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-30 18:45:44 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/useCases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const account: any = await accountCollection.insertOne(accountData)
+    const { _id, ...accountWithoutId } = account
+    return Object.assign({}, accountWithoutId, { id: _id })
+  }
+}
